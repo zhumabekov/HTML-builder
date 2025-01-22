7f34674e2e05c0ed22c6f58227fce01543f660e4
zhumabekov <30315366+zhumabekov@users.noreply.github.com>
2025-01-22 12:44:45 +0500

    fix: fix task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const textLink = path.join(__dirname, "text.txt");
+const readableStream = fs.createReadStream(textLink);
+
+readableStream.pipe(process.stdout);
